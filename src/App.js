58fc8448cb2d150f374af0e6b0858828d2e7b1f6
Dada <dadasahebkale.dk@gmail.com>
2022-04-22 10:32:57 +0530

    new prog

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Search from './components/Search';
 
+
+const UNSPLASH_KEY=process.env.REACT_APP_UNSPLASH_KEY;
 function App() {
+  const [word, setWord]=useState('');
+  const [images,setImages] =useState([]);
+  const handleSearchSubmit = (e) =>{
+    e.preventDefault();
+    console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) =>{
+        console.log(data);
+        setImages([daga, ...images])
+      })
+      .catch((err) =>{
+        console.log(err);
+      })
+      setWord(``);
+
+  }
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="Ganu's Beautiful Images Gallery"/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
